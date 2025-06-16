9a089db1cc152774a887bcec612a499d7a1caf67
YiZhiYuanYuan-qyy <73566575+YiZhiYuanYuan-qyy@users.noreply.github.com>
2025-06-16 21:56:58 +0800

    更新说明

---
+++ fullscreen.js
@@ -0,0 +1,11 @@
+
+document.addEventListener('click', () => {
+  const elem = document.documentElement;
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    elem.msRequestFullscreen();
+  }
+});
